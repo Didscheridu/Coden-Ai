311496c141353ef5ca904da99254a2dddcbf9499
Didscheridu <kayden.schunack@gmail.com>
2026-03-24 15:21:47 +0100

    Update Firebase configuration and imports

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -3,15 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebas
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
-// Deine exakte Firebase Konfiguration
+// Deine Firebase Konfiguration (Analytics/measurementId wurde für DSGVO entfernt!)
 const firebaseConfig = {
   apiKey: "AIzaSyAXszFYN8ablxO9ywdMk3LjzGBZXVorVQI",
   authDomain: "coden-4561f.firebaseapp.com",
   projectId: "coden-4561f",
   storageBucket: "coden-4561f.firebasestorage.app",
   messagingSenderId: "291880469153",
-  appId: "1:291880469153:web:d60f58eb21a21aa15e26e0",
-  measurementId: "G-V44PJ9ERXW"
+  appId: "1:291880469153:web:d60f58eb21a21aa15e26e0"
 };
 
 // Firebase initialisieren
